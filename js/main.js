38c6b17e3f0d7b00edb215698a4b4d1385b89b60
Irina <irinka-akva@ya.ru>
2022-10-21 22:06:54 +0300

    Выполняет домашнее задание 4 модуля

--- js/main.js
+++ js/main.js
@@ -17,3 +17,55 @@ function checkStringLength (currentString, maxLength) {
 checkStringLength('hello!', 100);
 
 
+const descriptionFoto = [
+  'Happy moments',
+  'My cat',
+  'Rainbow',
+  'Flowers',
+  'Good morning',
+  'Happyness',
+  'Sunrise',
+  'Best friends',
+  'Family',
+  'Surprise',
+  'Contemporary art',
+  'Animals',
+  'Interesting day',
+  'Dog',
+  'Successful photo',
+  'Rose',
+  'Memories',
+  'Pleasant atmosphere',
+  'Miracle',
+  'Fruits',
+  'Autumn',
+  'Rainy day',
+  'Trip to the village',
+  'Delicious pie',
+  'Beautiful cake'
+];
+
+const likesNumber = {
+  min: 15,
+  max: 200,
+};
+
+const PHOTOS_NUMBER = 25;
+
+const getRandomArrayElement = function (elements) {
+  return elements[getRandomPositiveInteger (0, elements.length - 1)];
+};
+
+const createFotoDescription = function (index) {
+  return {
+    id: index,
+    url: `photos/${index}.jpg`,
+    description: getRandomArrayElement(descriptionFoto),
+    likes: getRandomPositiveInteger (likesNumber.min, likesNumber.max),
+    comments: getRandomPositiveInteger (0, 200),
+  };
+};
+
+const fotoDescription = Array.from ({length: PHOTOS_NUMBER}, (_, pictureIndex) => createFotoDescription(pictureIndex + 1));
+
+fotoDescription();
